5fc4f6ebb122deda728a1c6251ff60b4cce51e2a
William Maphanga <williammaphanga@Williams-MacBook-Air.local>
2018-03-11 20:56:26 +0200

    Added Kanaban board three main lists sections Added Card component, List component and Checklist component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Kanbanboard from './components/kanbanboard/Kanbanboard';
+
+let cardsList = [ {
+  id: 1,
+  title: "Read the Book",
+  description: "I should read the whole book",
+  status: "in-progress",
+  tasks: []
+}, {
+  id: 2,
+  title: "Write some code",
+  description: "Code along with the samples in the book",
+  status: "todo",
+  tasks: [
+{
+id: 1,
+      name: "ContactList Example",
+done: true },
+{
+id: 2,
+      name: "Kanban Example",
+      done: false
+    },
+{
+id: 3,
+      name: "My own experiments",
+done: false }
+] },
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Kanbanboard cards={cardsList}/>
       </div>
     );
   }
