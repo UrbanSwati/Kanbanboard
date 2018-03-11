a67005e5f3b7c70ac56b3770049290e210074bb3
William Maphanga <williammaphanga@Williams-MacBook-Air.local>
2018-03-11 21:03:28 +0200

    Added listing format

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-
+import Card from './../card/Card';
 class List extends Component {
     render() {
+        var cards = this.props.cards.map((card) => {
+            return <Card id={card.id}
+                        title={card.title}
+                        description={card.description}
+                        task={card.task} />
+        })
         return (
-            <div>
-                
+            <div className="list">
+                <h1>{this.props.title}</h1>
+                {cards}
             </div>
         );
     }
