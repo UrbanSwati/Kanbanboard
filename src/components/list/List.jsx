e9efdb7a02bb0fa98e3f17df0f4ef4acb6828487
William Maphanga <williesdk@gmail.com>
2018-04-18 20:31:50 +0200

    Modified components

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import Card from './../card/Card';
 class List extends Component {
-    render() {
-        var cards = this.props.cards.map((card) => {
-            return <Card id={card.id}
-                        title={card.title}
-                        description={card.description}
-                        tasks={card.tasks} />
-        })
-        return (
-            <div className="list">
-                <h1>{this.props.title}</h1>
-                {cards}
-            </div>
-        );
-    }
+  render() {
+    var cards = this.props.cards.map(card => {
+      return (
+        <Card
+          id={card.id}
+          title={card.title}
+          description={card.description}
+          tasks={card.tasks}
+          key={card.id}
+        />
+      );
+    });
+    return (
+      <div className="list">
+        <h1>{this.props.title}</h1>
+        {cards}
+      </div>
+    );
+  }
 }
 
 export default List;
